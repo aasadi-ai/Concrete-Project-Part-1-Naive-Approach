f1926410dedbe6e4db249418fbefab66d199ff24
halifk <aasadi.ai.work@gmail.com>
2021-08-04 16:49:32 -0400

    Uploads first draft of project

---
+++ BaseModel.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split,GridSearchCV
+
+class BaseModel:
+    def __init__(self):
+        np.random.seed(13)
+        self.X,self.y = self.loadData()
+        self.X_train,self.X_validation,self.X_test,self.y_train,self.y_validation,self.y_test = self.split(self.X,self.y)
+        #Standardize Data
+        self.standardizationParams = {"mean":None,"std":None}
+        self.X_train = self.standardize(self.X_train,useParams=False)
+        self.X_validation = self.standardize(self.X_validation)
+        self.X_test = self.standardize(self.X_test)
+        #Build data for grid Search
+        self.X_grid = np.concatenate((self.X_train,self.X_validation))
+        self.y_grid = np.concatenate((self.y_train,self.y_validation))
+        #Init Class Specific Variables
+        self.modelType = None
+        self.hyperParameterSearchSpace = None
+
+    def loadData(self):
+        #Load Data  and Rename Columns
+        df = pd.read_csv("data/concreteData.csv")
+        df.columns = ["Cement","Slag","Ash","Water","Plasticizer","CoarseAgg","FineAgg","Age","CompressiveStrength"]
+        return np.array(df.iloc[:,:-1]),np.array(df["CompressiveStrength"])
+
+    def standardize(self,X,useParams=True):
+        if useParams:
+            return (X-self.standardizationParams["mean"])/self.standardizationParams["std"]
+        else:
+            self.standardizationParams["mean"]=X.mean(axis=0)
+            self.standardizationParams["std"]=X.std(axis=0)
+            return (X-self.standardizationParams["mean"])/self.standardizationParams["std"]
+  
+    def split(self,X,y):
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.30)
+        X_validation,X_test,y_validation,y_test = train_test_split(X_test,y_test,test_size=0.5)
+        return X_train,X_validation,X_test,y_train,y_validation,y_test
+    
+    def score(self,y,yHat):
+        rSquared = metrics.r2_score(y,yHat)
+        mae = metrics.mean_absolute_error(y,yHat)
+        mae_PercentOfMean = mae/np.mean(y)
+        return  [rSquared,mae,mae_PercentOfMean]
+
+    def baselineMean(self,X,y):
+        X_train,X_validation,X_test,y_train,y_validation,y_test = self.split(X,y)
+        return self.score(y_validation,np.full_like(y_validation,np.mean(y_train)))
+        
+    def baselineLinearRegression(self,X,y):
+        X_train,X_validation,X_test,y_train,y_validation,y_test = self.split(X,y)
+        testModel = linear_model.LinearRegression()
+        testModel.fit(X_train,y_train)
+        yHat = testModel.predict(X_validation)
+        return self.score(y_validation,yHat)
+    
+    def fit(self,X,y,hyperparameters=None):
+        if hyperparameters:
+            self.model = self.modelType(**hyperparameters)
+            self.model.fit(X,y)
+        else:
+            self.model = self.modelType()
+            self.model.fit(X,y)    
+
+    def predict(self,X):
+        return self.model.predict(X)
+
+    def getHyperparameters(self,hyperparameters=None,updateModel=True):
+        #Use random search to narrow down parameters to gridSearch
+        if hyperparameters:
+            gridSearcher = GridSearchCV(self.modelType(),hyperparameters)
+            gridSearcher.fit(self.X_grid,self.y_grid)
+        else:
+            gridSearcher = GridSearchCV(self.modelType(),self.hyperParameterSearchSpace)
+            gridSearcher.fit(self.X_grid,self.y_grid)
+
+        if updateModel:
+            self.model = gridSearcher.best_estimator_
+        return gridSearcher.best_params_,self.score(gridSearcher.best_estimator_.predict(self.X_validation),self.y_validation)
+
+
+
+
+
+
+#Check Values and NaNs
+# df.isna().sum()
+# df.apply(pd.Series.value_counts).T.stack()
+#calculate z1 score and bin columns with outliers that are 3 or -3 std from mean
+
+# %%
---
+++ Models.py
@@ -0,0 +1,44 @@
+#%%
+import numpy as np
+import pandas as pd
+from BaseModel import BaseModel
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+class SVM(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.modelType = SVR
+        self.model = None
+        self.hyperParameterSearchSpace = {
+            "kernel": ["linear","rbf"],
+            "degree":[3],
+            "C":[0.01,1]
+        }
+
+class Linear(BaseModel):
+     def __init__(self):
+        super().__init__()
+        self.modelType = Ridge
+        self.model = None
+        self.hyperParameterSearchSpace = {"alpha":[i/10 for i in range(0,10,1)]}
+
+class DecisionTree(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.modelType = DecisionTreeRegressor
+        self.model = None
+        self.hyperParameterSearchSpace = {
+            "max_depth":[i for i in range(1,16)],
+            "min_samples_leaf":[i for i in range(1,10)]
+        } 
+
+class KNN(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.modelType = KNeighborsRegressor
+        self.model = None
+        self.hyperParameterSearchSpace = {"n_neighbors":[i for i in range(1,11)]}     
+# %%
