8d99ccd3011c415c582c398c524550b262ca6dea
aasadi-ai <84348393+aasadi-ai@users.noreply.github.com>
2021-08-04 23:24:11 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#%%
+import numpy as np
+import pandas as pd
+from Models import models
+
+def getScores():
+    output = []
+    for model in models.keys():
+        currentModel = models[model]()
+        bestParams,validationMetrics = currentModel.getHyperparameters()
+        output.append({
+            "ModelType:":model,
+            "ModelParams:":bestParams,
+            "ModelValidationMetrics:":validationMetrics,
+            "EstimatedPerformance:": currentModel.estimatePerformance()
+        })
+    return output
+# %%
