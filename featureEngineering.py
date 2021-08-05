e35d7c23fe2a760879f2d72d7d3ae662d60194b7
halifk <aasadi.ai.work@gmail.com>
2021-08-05 15:27:54 -0400

    Adds feature creation

--- featureEngineering.py
+++ featureEngineering.py
@@ -18,5 +18,21 @@ def findAndRemoveOutliers(df,zThreshhold=2.5):
     mask = (np.abs(X) <= zThreshhold).all(1)
     return df[mask].iloc[:,:-1],df[mask]["CompressiveStrength"],df[mask]
 
-X,y,df = loadData()
+def featureCreation(df):
+    outputDF = df
+    columns = df.columns.tolist()[:-1]
+    for column0 in columns:
+        print(column0)
+        outputDF[f"{column0}_Sqrd"] = np.square(df[column0])
+        for column1 in columns:
+            if column0!=column1:
+                outputDF[f"{column0}/{column1}"] = df[column0]/(df[column1]+0.1)
+                outputDF[f"{column0}*{column1}"] = df[column0]*df[column1]
+    return outputDF
+
+# X,y,df = loadData()
+# df = transformFeatures(df)
+# _,_,df = findAndRemoveOutliers(df)
+# df = featureCreation(df)
+# df.head()
 # %%
